bb7e5cd422035180fc3ac44c346ba5a1f95e7589
jeymarherrera <jeymarherrera@gmail.com>
2023-02-06 21:42:14 -0500

    git notes

---
+++ practicas/practica-git/git.js
@@ -0,0 +1,23 @@
+// // 1. configurar nombre
+// // git config --global user.name “[firstname lastname]”
+// // #set a name that is identifiable for credit when review version history
+
+// 2. configurar correo
+// git config --global user.email “[valid-email]”
+// #set an email address that will be associated with each history marker
+
+// 3. configurar coloreado de la linea de comandos
+// git config --global color.ui auto
+// #set automatic command line coloring for Git for easy reviewing
+
+// 4. inicializar un repositorio
+// git init
+// #initialize an existing directory as a Git repository
+
+// 5. clonar un repositorio
+// git clone [url]
+// # retrieve an entire repository from a hosted location via URL
+
+// 6. git status
+// git status
+// # show modified files in working directory, staged for your next commit
